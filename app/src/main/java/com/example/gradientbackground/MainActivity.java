192244ffea780549cd80ee3ec223d260d42264db
Asadu <techasadbd@gmail.com>
2023-09-11 13:48:04 +0600

    Initial commit

---
+++ app/src/main/java/com/example/gradientbackground/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.gradientbackground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.buttonId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button is Clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
